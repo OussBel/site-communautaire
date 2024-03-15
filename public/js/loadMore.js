ddb16beeceaee191cd3d1105d015a1119473edaf
Oussama Bel <oussamabelfarhi@gmail.com>
2024-03-15 15:01:03 +0100

    Ajout du bouton télécharger plus

---
+++ public/js/loadMore.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let i = 15;
+    const loadMoreButton = document.querySelector('#loadMoreButton');
+    const cards = document.querySelectorAll('.card');
+
+    loadMoreButton.addEventListener('click', () => {
+        i += 15; // Increment i by 5 to display the next 5 cards
+        displayMoreCards(i);
+    });
+
+    const displayMoreCards = (i) => {
+        cards.forEach((card, index) => {
+
+            if (index < i) {
+                card.style.display = '';
+            } else {
+                card.style.display = 'none';
+            }
+
+            if (i >= cards.length) {
+                loadMoreButton.style.display = 'none'; // Hide the button if there are no more cards
+            } else {
+                loadMoreButton.style.display = ''; // Show the button if there are more cards
+            }
+
+        });
+    };
+
+    displayMoreCards(i);
+});
